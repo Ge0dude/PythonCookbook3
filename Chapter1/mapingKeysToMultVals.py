b51fad48327ecfabe4eaab2b31db3a33b0dba9e0
Brendon <btucker1992@gmail.com>
2017-04-28 15:53:59 +0800

    useful information--good for no internet times (aka plane and boat rides)

--- Chapter1/mapingKeysToMultVals.py
+++ Chapter1/mapingKeysToMultVals.py
@@ -5,6 +5,7 @@ Created on Fri Apr 28 11:55:34 2017
 
 @author: brendontucker
 """
+from collections import OrderedDict
 
 d = {
 'a' : [1, 2, 3], 'b' : [4, 5]
@@ -15,9 +16,24 @@ e = {
 }
 
 '''here choice of dict vs list has to do with whether multiple values
-need to be kept, as well as order of entry'''
+need to be kept, as well as order of entry. This is part of why I have a hard
+time remembering what to do with dictionaries--there are many methods to choose
+to alter them, depending on the internal data'''
 
 d['a'].append(3)
 # print(d['a']) -> [1, 2, 3, 3]
 e['a'].add(3)
 # print(e['a']) -> {1, 2, 3}
+
+        
+'''default dict can also be useful, preserves the insertion order'''
+
+order = OrderedDict()
+
+order['whyy'] = 12
+order['second'] = 0
+order['apple'] = 2
+order['bananas'] = 1
+
+for key in order:
+    print(key, order[key])
