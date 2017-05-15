0cbb5657c489ce40452fab7de6aa8d736808da13
Brendon <btucker1992@gmail.com>
2017-05-15 08:22:31 +0800

    dictionaries

--- Chapter1/1.8CalculatingWithDict.py
+++ Chapter1/1.8CalculatingWithDict.py
@@ -16,3 +16,23 @@ prices = {
 
 #often useful to invert teh keys and values of the dictionary 
 #this can be done with the zip() function
+
+#necessary because this doesn't do what we want, unless we're intrested in 
+#lexicographic order...which would be odd for stocks
+
+print(min(prices)) # Returns 'AAPL' 
+print(max(prices)) # Returns 'IBM'
+
+#the following is closer, but still not quite right
+
+print(min(prices.values())) # Returns 10.75 
+print(max(prices.values())) # Returns 612.78
+
+#there are alternative methods that do work but are a bit complicated
+
+print(min(prices, key=lambda k: prices[k])) # Returns 'FB' 
+print(max(prices, key=lambda k: prices[k])) # Returns 'AAPL'
+
+#just to understand that better...
+
+minVal = min(prices.values())
