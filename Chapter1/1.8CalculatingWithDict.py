ffd890157e56d018d14ccca79b10c51bcfd130de
Brendon <btucker1992@gmail.com>
2017-05-15 08:01:54 +0800

    dict dict dict zip zip zip

---
+++ Chapter1/1.8CalculatingWithDict.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 15 07:48:09 2017
+
+@author: brendontucker
+"""
+
+prices = {
+   'ACME': 45.23,
+   'AAPL': 612.78,
+   'IBM': 205.55,
+   'HPQ': 37.20,
+   'FB': 10.75
+}
+
+#often useful to invert teh keys and values of the dictionary 
+#this can be done with the zip() function
